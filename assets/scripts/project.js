350e671af5b904d17fc11b67c6284d0cab05894e
MayChant <annied617@gmail.com>
2016-07-12 20:30:23 -0400

    Create project.js

---
+++ assets/scripts/project.js
@@ -0,0 +1,12 @@
+//////////////////////
+// Showcase control //
+//////////////////////
+$("#showcase_control_prev").click(function(){
+	$("#media_1").toggleClass("hidden");
+	$("#media_2").toggleClass("hidden");
+});
+
+$("#showcase_control_next").click(function(){
+	$("#media_1").toggleClass("hidden");
+	$("#media_2").toggleClass("hidden");
+});
