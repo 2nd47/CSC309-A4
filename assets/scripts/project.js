26dfbb914f8012f9cdc403b2636343d04e6693c3
MayChant <annied617@gmail.com>
2016-07-13 23:49:33 -0400

    Update project.js

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -10,3 +10,16 @@ $("#showcase_control_next").click(function(){
 	$("#media_1").toggleClass("hidden");
 	$("#media_2").toggleClass("hidden");
 });
+
+///////////////////
+var following = false;
+$("#profile_follow").click(function(){
+	console.log("click");
+	following = !following;
+	if (following) {
+		$("#profile_follow").html("Unfollow");
+	}
+	else {
+		$("#profile_follow").html("Follow");
+	}
+});
