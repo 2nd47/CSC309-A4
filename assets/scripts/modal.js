f4588ac00d2bbf10570687d550682367191f983d
Alexander <Alexandermastryukov@gmail.com>
2016-07-13 22:28:03 -0400

    Adjusted login popup
    - Made the background fade darker to really bring out the login
    - Simplified the login popup to look more sleek and less bulky
    - Fixed email field being password field
    - Fixed verify password field being a text field
    - Adjusted some fonts to be smaller
    - Removed close button (we don't need it if the user can click outside
    the box)

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -4,9 +4,6 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("login_nav");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 var signupSwitch = document.getElementById("signup_switch");
 var loginSwitch = document.getElementById("login_switch");
 
@@ -16,13 +13,6 @@ var loginButton = document.getElementById("login_button");
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-    document.getElementById("login_signup_content").style.display = "block";
-    document.getElementById("verify_content").style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
