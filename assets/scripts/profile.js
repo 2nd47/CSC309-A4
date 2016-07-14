c6d5eed7ea0eaaa5bce72d8b24d54cf99eadab0e
MayChant <annied617@gmail.com>
2016-07-13 21:06:22 -0400

    Editable profile

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -9,3 +9,19 @@ $("#profile_follow").click(function(){
 		$("#profile_follow").html("Follow");
 	}
 });
+
+///////////////////////////////////////////
+$("#profile_bio_button").click(function(){
+	$("#profile_bio_edit").removeClass("hidden");
+	$("#profile_bio_save").removeClass("hidden");
+	$("#profile_bio_content").addClass("hidden");
+	$("#profile_bio_button").addClass("hidden");
+});
+
+$("#profile_bio_save").click(function(){
+	$("#profile_bio_content").html($("#profile_bio_editor").val());
+	$("#profile_bio_edit").addClass("hidden");
+	$("#profile_bio_save").addClass("hidden");
+	$("#profile_bio_content").removeClass("hidden");
+	$("#profile_bio_button").removeClass("hidden");
+});
