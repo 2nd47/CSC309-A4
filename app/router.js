ec06c1b096759eec0d73cb5d6f36bb7eaa1fe4f1
2nd47 <2nd4777@gmail.com>
2016-07-16 17:31:41 -0400

    Server routing
    Created some new routes for the app to use after discussion

--- app/router.js
+++ app/router.js
@@ -12,8 +12,19 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
+// change the request methods as required
+// refer to express documentation for more details
+
 // if logged in: feed; else: landing page
 router.get('/', function (req, res, next) {
+  res.send('AIDA Home Page!');
+});
+
+router.post('/login', function (req, res, next) {
+  res.send('AIDA Home Page!');
+});
+
+router.post('/signup', function (req, res, next) {
   res.send('AIDA Home Page!');
 });
 
@@ -21,8 +32,35 @@ router.get('/', function (req, res, next) {
 router.get('/contracts', function (req, res) {
 });
 
+// create a new contract
+router.get('/contracts/new', function (req, res) {
+});
+
+// details of contract with contract_id
+router.get('/contracts/:contract_id', function (req, res) {
+});
+
 // list of profiles
 router.get('/people', function (req, res) {
+});
+
+// details of people with user_id
+router.get('/people/:user_id', function (req, res) {
+});
+
+// list of projects
+router.get('/projects', function (req, res, next) {
+  res.send('AIDA Home Page!');
+});
+
+// create a new project
+router.get('/projects/new', function (req, res, next) {
+  res.send('AIDA Home Page!');
+});
+
+// details of project with project_id
+router.get('/projects/:project_id', function (req, res, next) {
+  res.send('AIDA Home Page!');
 });
 
 // message inbox
