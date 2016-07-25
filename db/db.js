7327496ce3c9699592a36e39fc7c2f9f7235d4b7
2nd47 <2nd4777@gmail.com>
2016-07-24 20:46:50 -0400

    Various db helper functions
    - get user, contract, and project by id
    - get user and project by name
    - get contract by price range
    
    so far, all of these return the entire document; this will change in the
    future to only return what is exactly necessary

--- db/db.js
+++ db/db.js
@@ -14,5 +14,44 @@ var connect = function(callback) {
   });
 }
 
+// Get individual user by searching for ID
+var getUser = function(id) {
+  return models.User.findById(id);
+}
+
+// Get individual user by searching for username
+var getUserByUsername = function(username) {
+  return models.User.findOne({
+    'username' : username
+  });
+}
+
+// Get individual project by searching for ID
+var getProject = function(id) {
+  return models.Project.findById(id);
+}
+
+// Get individual project by searching for name
+var getProjectByName = function(name) {
+  return models.Project.findOne({
+    'name' : name
+  })
+}
+
+// Get individual contract by searching for ID
+var getContract = function(id) {
+  return models.Contract.findById(id);
+}
+
+
+// Get contracts by price range
+var getContractsByPrice = function(lowlimit, highlimit) {
+  return models.Contract.
+    find().
+    where('budget').gt(lowlimit).lt(highlimit).
+    exec();
+}
+
+
 module.exports.models = models;
 module.exports.connect = connect;
