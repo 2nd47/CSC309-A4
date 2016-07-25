c37e6358e441ad92a46ad1cc490829a0e1928718
2nd47 <2nd4777@gmail.com>
2016-07-25 19:02:08 -0400

    Added commented connection code for Heroku

--- db/db.js
+++ db/db.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 
 var models = require("./models.js");
 
-var url = 'mongodb://localhost/appdb'
+/*
+var dbUser = 'aida-master';
+var dbPassword = 'dbpassword';
+var url = 'mongodb://' +
+  dbUser + ':' +
+  dbPassword +
+  '@ds029735.mlab.com:29735/heroku_qdmfvghp'
+*/
+
+var url = 'mongodb://localhost/appdb';
 
 var connect = function(callback) {
   mongoose.connect(url);
