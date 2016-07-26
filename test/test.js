a29eecb1fd3141be447d9ceb8ca574a505fe4ba5
2nd47 <2nd4777@gmail.com>
2016-07-26 01:54:47 -0400

    Added a few basic tests
    - Check if saving a user to the database works

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
+var assert = require('chai').assert;
+var db = require('../db/db');
+var router = require('../app/router.js');
+
 before(function() {
   // runs before all tests in this block
 });
@@ -13,3 +17,32 @@ beforeEach(function() {
 afterEach(function() {
   // runs after each test in this block
 });
+
+// Some variables that are common among tests
+var username = 'testman77';
+var password = 'testpass';
+var email = 'testman77@email.org';
+
+// Enter a new user into the database without error
+describe('User', function() {
+  before(function() {
+    db.models.user.find({username:username}).remove();
+  });
+  describe('#save()', function() {
+    it('should create a new user', function(done) {
+      var user = new User({
+        username: username,
+        passwordHash:password,
+        email: email
+      });
+      user.save(done);
+    });
+  });
+});
+
+// Attempt to sign up with appropriate information
+/*
+describe('router', function() {
+
+});
+*/
