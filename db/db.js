d43d78460f1043830db72e1aac09b3aa6a16f681
2nd47 <2nd4777@gmail.com>
2016-07-30 19:11:55 -0400

    db uses env variable to connect to db or localhost

--- db/db.js
+++ db/db.js
@@ -1,15 +1,7 @@
 var mongoose = require('mongoose');
 
 var models = require("./models.js");
-
-var dbUser = 'aida-master';
-var dbPassword = 'dbpassword';
-var url = 'mongodb://' +
-  dbUser + ':' +
-  dbPassword +
-  '@ds029735.mlab.com:29735/heroku_qdmfvghp'
-
-//var url = 'mongodb://localhost/appdb';
+var url = process.env.MONGODB_URI || 'mongodb://localhost/appdb';
 
 var connect = function(callback) {
   mongoose.connect(url);
